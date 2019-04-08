c9cbf937b98379e37bb55c0cf39630bcbca9f7f3
Stella McEwen <mcewen.stella@gmail.com>
2019-04-08 00:29:40 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,30 @@
+def instantiate_new_array
+  my_new_array = []
+end
+def array_with_two_elements
+  star_trek = ["kirk", "spock"]
+end
+def first_element(array)
+  array = ["Welcome to New York", "ugh"]
+  array[0]
+end
+def third_element(array)
+  array = ["whoever", "designed this", "has no", "Style"]
+  array[3]
+end
+def last_element(array)
+  array = ["good luck", "ever getting", "Out of The Woods"]
+  array[-1]
+end
+def first_element_with_array_methods(array)
+  array = ["Thailand", "is great", "I'm sure"]
+  array.first
+end
+def last_element_with_array_methods(isitfar)
+  isitfar = ["I've never been", "to", "Myanmar"]
+  isitfar.last
+end
+def length_of_array(array)
+  array = ["Space", "the", "final", "frontier", "These", "are", "the", "voyages"]
+  array.length 
+end
