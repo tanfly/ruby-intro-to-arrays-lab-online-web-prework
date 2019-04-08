838af6462d17e5c2ac804b677754964218bc75f3
Stella McEwen <mcewen.stella@gmail.com>
2019-04-08 00:16:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,6 +5,6 @@ def array_with_two_elements
   star_trek = ["kirk", "spock"]
 end
 def first_element(array)
-  dr_suess = ["thing1", "thing2"]
-  dr_suess[0]
+  array = ["thing1", "thing2"]
+  array[0]
 end
